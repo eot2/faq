076a479a26e87eda34658fd1899acab7eadf977a
eot2 <eot2@njit.edu>
2018-07-23 20:15:15 -0400

    Fixed branding link to correctly direct logged in user to their home page

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -3,7 +3,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-07-22 21:10:34.
+    * Generated on 2018-07-23 14:27:38.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
